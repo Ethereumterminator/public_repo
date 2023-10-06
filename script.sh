688749816703e0564d2edf3b5248ac544c8dc68e
DxtorJa <125950957+DxtorJa@users.noreply.github.com>
2023-10-06 10:13:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,17 +6,23 @@ sleep 3
 echo "e-mail -> liyanagelsofficial@gmail.com"
 sleep 1
 echo "Installing"
+
 # Run the Docker container in the background
 apt update -y
-
 apt upgrade -y
-
 apt install docker.io -y
-
 ufw disable -y
 
-docker run -d --cap-add=NET_ADMIN -p 1194:1194/udp -p 80:8080/tcp -p 81:81/tcp -p 5050:5050/tcp -p 5051:5051/tcp -p 5052:5052/tcp -p 5053:5053/tcp -p 5054:5054/tcp -p 5055:5055/tcp -e HOST_ADDR=$(curl -s https://api.ipify.org) --name dockovpn alekslitvinenk/openvpn
+# Prompt the user to enter forwarding ports separated by spaces
+read -p "Enter forwarding ports (e.g., 81 5050 5051): " ports
 
+# Construct a string for port mappings
+port_mappings=""
+for port in $ports; do
+    port_mappings+="-p $port:$port/tcp "
+done
+
+docker run -d --cap-add=NET_ADMIN -p 1194:1194/udp -p 80:8080/tcp $port_mappings -e HOST_ADDR=$(curl -s https://api.ipify.org) --name dockovpn alekslitvinenk/openvpn
 
 echo "For downloading the OVPN file, please visit: http://ip:80 then press Y"
 sleep 1
@@ -27,14 +33,8 @@ read -p "Type 'Y' to continue: " response
 if [ "$response" = "Y" ] || [ "$response" = "y" ]; then
     echo "Continuing..."
 
-    # Prompt the user to enter a list of forwarding ports separated by spaces
-    read -p "Enter forwarding ports (e.g., 81 5050 5051): " ports
-
-    # Split the user input into an array
-    IFS=" " read -ra port_array <<< "$ports"
-
     # Loop through the array and run the docker exec command for each port
-    for port in "${port_array[@]}"; do
+    for port in $ports; do
         docker exec -it dockovpn iptables -t nat -A PREROUTING -i eth0 -p tcp --dport "$port" -j DNAT --to-destination 10.8.0.6:"$port"
     done
 
